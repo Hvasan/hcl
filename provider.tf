59795bf6c1136573784555a7340be4bca18c470f
Hanamantagouda Vasan <vasan.hanamant@gmail.com>
2025-04-26 17:54:48 +0530

    hcl-test

--- provider.tf
+++ provider.tf
@@ -12,5 +12,7 @@ terraform {
 
 provider "aws" {
   alias  = "default"
+  access_key = "AKIAX3NVJFI7A2EGPR7J"
+  secret_key = "WWwV9V+v0L3WmG5/J2UQMbt6y8OUxBUWZ4JRCtDo"
   region     = "us-east-1" # Change to your desired region
 }
